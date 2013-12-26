574a628665ae9f03166c6b8c806c597c2d418221
guitargodd97 <Hunter Heidenreich@10.0.0.4>
2013-12-25 23:26:02 -0500

    Updated Big Dog and Little Dog movement

--- Boney-Desura-and-Play/src/com/boney/desura/dogs/BigDog.java
+++ Boney-Desura-and-Play/src/com/boney/desura/dogs/BigDog.java
@@ -1,5 +1,7 @@
 package com.boney.desura.dogs;
 
+import java.util.Random;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
@@ -8,11 +10,14 @@ public class BigDog extends GenericDog {
 	private int animationBuffer;
 	private int currentFrame;
 	public boolean switchSprite;
+	private boolean mid;
 
 	public BigDog() {
 		super(128, 128, pics[0], 2);
 		pntWorth = 100;
 		switchSprite = false;
+		Random ran = new Random();
+		mid = ran.nextBoolean();
 	}
 
 	public void move() {
@@ -52,17 +57,34 @@ public class BigDog extends GenericDog {
 	}
 
 	public boolean normalGetDone() {
-		if (side >= 0.5) {
-			if (location.x > Gdx.graphics.getWidth() + 10) {
-				xVelocity *= -2;
-				animationBuffer = 3;
-				return true;
+		if (mid) {
+			if (side >= 0.5) {
+				if (location.x > Gdx.graphics.getWidth() + 10) {
+					xVelocity *= -2;
+					animationBuffer = 3;
+					return true;
+				}
+			} else {
+				if (location.x < 0 - shape.x - 10) {
+					xVelocity *= -2;
+					animationBuffer = 3;
+					return true;
+				}
 			}
 		} else {
-			if (location.x < 0 - shape.x - 10) {
-				xVelocity *= -2;
-				animationBuffer = 3;
-				return true;
+			Gdx.app.log("Swithc", "Otha side");
+			if (side >= 0.5) {
+				if (location.x > (Gdx.graphics.getWidth() / 2) + 10) {
+					xVelocity *= -2;
+					animationBuffer = 3;
+					return true;
+				}
+			} else {
+				if (location.x < (Gdx.graphics.getWidth() / 2) - shape.x - 10) {
+					xVelocity *= -2;
+					animationBuffer = 3;
+					return true;
+				}
 			}
 		}
 		return false;
--- Boney-Desura-and-Play/src/com/boney/desura/dogs/SmallDog.java
+++ Boney-Desura-and-Play/src/com/boney/desura/dogs/SmallDog.java
@@ -7,10 +7,12 @@ public class SmallDog extends GenericDog {
 	public static Sprite[] pics = new Sprite[10];
 	private int animationBuffer;
 	private int currentFrame;
+	private int jumped;
 
 	public SmallDog() {
 		super(64, 64, pics[0], 0);
 		pntWorth = 25;
+		jumped = 0;
 	}
 
 	public void move() {
@@ -29,19 +31,23 @@ public class SmallDog extends GenericDog {
 
 	@Override
 	public void specialty() {
-		int i = (int) (Math.random() * 10);
-		if (location.y == floor && i % 3 == 0)
-			yVelocity = 350.0f;
-		// If below the floor, push up
-		if (location.y < floor) {
-			location.y = floor;
-			yVelocity = 0;
-		}
-		// If above the floor, apply velocity
-		if (location.y >= floor) {
-			location.y += yVelocity * Gdx.graphics.getDeltaTime();
-			if (location.y > floor)
-				yVelocity -= gravity * Gdx.graphics.getDeltaTime();
+		if (location.x > 25 && location.x + shape.x < 750) {
+			if (jumped == 0) {
+				yVelocity = 350.0f;
+				jumped = 1;
+			}
+			// If below the floor, push up
+			if (location.y < floor && jumped == 2) {
+				location.y = floor;
+				yVelocity = 0;
+			}
+			// If above the floor, apply velocity
+			if (location.y >= floor && jumped == 1) {
+				location.y += yVelocity * Gdx.graphics.getDeltaTime();
+				if (location.y > floor)
+					yVelocity -= gravity * Gdx.graphics.getDeltaTime();
+			} else
+				jumped = 2;
 		}
 	}
 
